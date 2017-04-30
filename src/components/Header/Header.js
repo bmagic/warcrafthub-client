0558de84417704727357a8e16d577f579857e0e3
Alogon <ae.alogon@gmail.com>
2017-04-30 20:36:24 +0200

    Header²

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { IndexLink } from 'react-router'
+
 import LoadingBar from 'react-redux-loading-bar'
+import SearchBar from '../../components/SearchBar'
+import SideBarMenu from '../../components/SideBarMenu'
+import SideBarAccount from '../../components/SideBarAccount'
+
 import config from '../../config'
+
+import logo from './assets/logo.png'
 
 import './Header.scss'
 
@@ -11,46 +18,56 @@ class Header extends React.Component {
   constructor (props) {
     super(props)
 
-    this.toggle = this.toggle.bind(this)
     this.state = {
-      isOpen: false
+      searchBar: false,
+      sideBarMenu: false,
+      sideBarAccount: false,
     }
+
+    this.toggleSearchBar = this.toggleSearchBar.bind(this);
+    this.toggleSideBarMenu = this.toggleSideBarMenu.bind(this);
+    this.toggleSideBarAccount = this.toggleSideBarAccount.bind(this);
   }
 
-  toggle () {
+  toggleSearchBar() {
     this.setState({
-      isOpen: !this.state.isOpen
-    })
+      searchBar: !this.state.searchBar
+    });
+  }
+
+  toggleSideBarMenu() {
+    document.body.classList.toggle('sidebar-open');
+    this.setState({
+      sideBarMenu: !this.state.sideBarMenu
+    });    
+  }
+
+  toggleSideBarAccount() {
+    document.body.classList.toggle('sidebar-open');
+    this.setState({
+      sideBarAccount: !this.state.sideBarAccount
+    });    
   }
 
   render () {
     return (
       <div>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <IndexLink to='/'>
-                Warcrafthub
-              </IndexLink>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <LinkContainer to='/counter'>
-                <NavItem >Counter</NavItem>
-              </LinkContainer>
-              <LinkContainer to='/character/eu/Archimonde/Alogon'>
-                <NavItem >Charact</NavItem>
-              </LinkContainer>
-              <LinkContainer to='/character/eu/Archimonde/Errrr'>
-                <NavItem >Error</NavItem>
-              </LinkContainer>
-              <NavItem href={config.API_ROOT + 'users/auth/bnet'}>Login with bnet</NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <LoadingBar className='loading' />
+        <header className="header">
+          <a href="#" className="col" id="btn-open-navig" onClick={this.toggleSideBarMenu}><span><i className="fa fa-bars fa-xl"></i></span><span className="title">menu</span></a>
+          <a href="home.html" className="logo"><img src={logo} alt="" /></a>
+          <div className="col-vide"></div>
+          <a href="#" className="col" onClick={this.toggleSearchBar}><span><i className="fa fa-search fa-xl"></i></span><span className="title">Recherche</span></a>
+          <a href="#" className="col btn-open-navig-compte" onClick={this.toggleSideBarAccount}>
+            <span>
+              <i className="fa fa-user fa-xl"></i>
+            </span>
+            <span className="title">compte</span>
+            <span className="loggee"><i className="fa fa-circle"></i></span>
+          </a>
+        </header>
+        <SideBarMenu toggleAction={this.toggleSideBarMenu} isOpen={this.state.sideBarMenu} />
+        <SideBarAccount toggleAction={this.toggleSideBarAccount} isOpen={this.state.sideBarAccount} />
+        <SearchBar isOpen={this.state.searchBar} />
       </div>
     )
   }
